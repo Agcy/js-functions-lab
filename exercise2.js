34764d652db69130bd05695b6adedbacff23be1d
765407782@qq.com <765407782@qq.com>
2023-09-19 10:37:14 +0100

    Exercise 2.

--- exercise2.js
+++ exercise2.js
@@ -1,1 +1,9 @@
-console.log(todoTitles)
+fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(response => response.json())
+    .then(json => {
+        const todoTitles = json.map(todo => todo.title)
+        console.log(todoTitles)
+    })
+    .catch(err => {
+        console.log(err)
+    })
